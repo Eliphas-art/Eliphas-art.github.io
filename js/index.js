9832d5cdcdee5e1a6ef059d47e3e0b070ee3929c
Eliphas-art <52660922+Eliphas-art@users.noreply.github.com>
2020-09-25 13:29:11 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// let obj = {
+//     item1: 1,
+//     item2: [123, "two", 3.0],
+//     item3:"hello",
+//     item4:"triki"
+// };
+//
+// let serialObj = JSON.stringify(obj); //сериализуем его
+// localStorage.setItem("myKey", serialObj); //запишем его в хранилище по ключу "myKey"
+// let returnObj = JSON.parse(localStorage.getItem("myKey")); //спарсим его обратно объект
+//
+// console.log(returnObj.item2);
+// obj.item2 = returnObj.item2;
+// obj.item2.push("trula");
+// console.log(returnObj.item2);
+// serialObj = JSON.stringify(obj); //сериализуем его
+// localStorage.setItem("myKey", serialObj);
+// returnObj = JSON.parse(localStorage.getItem("myKey"));
+// console.log(returnObj);
+//
